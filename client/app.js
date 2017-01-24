6f8ff830427eae03104b16010176da6dfd2442b8
James Byrum <me@jimmybyrum.com>
2017-01-23 22:54:55 -0800

    setting up app stuff

--- client/app.js
+++ client/app.js
@@ -1,4 +1,35 @@
 (function() {
+  var baseTitle = 'Slide Therapy 2017';
+
+  Crosshatch.route({
+    url: '/',
+    pattern: /^#?\!?\/?$/,
+    controller: function(self, url) {
+      Crosshatch.setTitle(baseTitle);
+    }
+  });
+
+  Crosshatch.route({
+    url: '/templates',
+    pattern: /^\/templates/,
+    controller: function(self, url) {
+      Crosshatch.setTitle(baseTitle + ' Templates');
+      scrollTo('#templates');
+    }
+  });
+
+  Crosshatch.route({
+    url: '/buy',
+    pattern: /^\/buy\/([\d\w\s+_\-%,"']*)/,
+    controller: function(self, url) {
+      var deck = url.replace(self.pattern, '$1');
+      Crosshatch.setTitle(baseTitle + ': Buy ' + deck);
+      scrollTo('#templates');
+    }
+  });
+
+  Crosshatch.ready();
+
   var checkout = StripeCheckout.configure({
     key: 'pk_test_CK71Laidqlso9O9sZDktqW6a',
     image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
@@ -6,8 +37,9 @@
     token: onToken
   });
 
-  $('.buy').on('click', initPurchase);
-  $('.email').on('click', email);
+  $(document).on('click', '.buy', initPurchase);
+  $(document).on('click', '.email', email);
+  $(document).on('click', '.scroll-to', onScrollToClick);
   $(window).on('popstate', checkout.close);
 
   function initPurchase(e) {
@@ -20,7 +52,7 @@
   }
 
   function onToken(token) {
-    console.warn(token);
+    // console.warn(token);
     $.ajax({
       type: 'POST',
       url: 'http://localhost:7678/api/charge',
@@ -43,6 +75,15 @@
         oid: 13
       }
     });
+  }
+
+  function onScrollToClick(e) {
+    var href = $(e.target).attr('href');
+    scrollTo(href.replace('#!/', '#'));
+  }
+
+  function scrollTo(id) {
+    $(window).scrollTo(id, 250, 'easeInOut');
   }
 
   function onError(error) {
