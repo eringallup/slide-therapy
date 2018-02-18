45af6215b90333dd235edca4f4177cb563967135
James Byrum <me@jimmybyrum.com>
2018-02-18 23:04:16 +0100

    fix logo to use Link

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,7 +11,7 @@ export default class Header extends React.Component {
     return <header className="container">
       <div className="row">
         <div className="col-sm-5">
-          <h1><a href="/"><img className="img-fluid" src="//d380dcsmppijhx.cloudfront.net/images/SlideTherapyLogo.png" alt="Slide Therapy" width="300" height="72"/></a></h1>
+          <h1><Link to="/"><img className="img-fluid" src="//d380dcsmppijhx.cloudfront.net/images/SlideTherapyLogo.png" alt="Slide Therapy" width="300" height="72"/></Link></h1>
         </div>
         <div className="col-sm-7 d-flex align-items-center justify-content-center justify-content-sm-end">
           <nav className="main-nav nav">
