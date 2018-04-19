af7e845eb6ca66e581b7cbe560603b9034d23a08
James Byrum <me@jimmybyrum.com>
2018-04-19 16:40:48 +0200

    update privacy policy for enabling behavioral tracking

--- client/components/Privacy.js
+++ client/components/Privacy.js
@@ -12,12 +12,13 @@ export default class Privacy extends React.Component {
     analytics.page('Privacy')
   }
   render () {
+    // https://termsfeed.com/blog/privacy-policy-google-analytics/
     return <div id="privacy" className="bg-light">
       <div className="container">
         <div className="row">
           <div className="col col-md-10 offset-md-1 col-lg-8 offset-lg-2 my-4 bg-white py-4 px-5">
             <h2 className="h4">Slide Therapy Privacy Policy</h2>
-            <p><cite style={{fontStyle: 'normal'}}>Effective: February 7, 2018</cite></p>
+            <p><cite style={{fontStyle: 'normal'}}>Effective: April 19, 2018</cite></p>
             <p>This privacy policy has been compiled to better serve those who are concerned with how their &apos;Personally Identifiable Information&apos; (PII) is being used online. PII, as described in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.</p>
             <h3>What personal information do we collect from the people that visit our website?</h3>
             <p>When ordering on our site, as appropriate, you may be asked to enter your name, email address, mailing address, credit card information or other details to help you with your experience.</p>
@@ -47,16 +48,29 @@ export default class Privacy extends React.Component {
             <p>You can choose to have your computer warn you each time a cookie is being sent, or you can choose to turn off all cookies. You do this through your browser settings. Since browser is a little different, look at your browser&apos;s Help Menu to learn the correct way to modify your cookies.</p>
             <h3>If users disable cookies in their browser:</h3>
             <p>If you turn cookies off, you will not be able to make purchases on this site.</p>
+            <h3>Do Not Track (DNT)</h3>
+            <p>We honor Do Not Track signals and do not track, set cookies, or use advertising when a Do Not Track (DNT) browser mechanism is in place.</p>
             <h3>Third-party disclosure</h3>
             <p>We do not sell, trade, or otherwise transfer to outside parties your Personally Identifiable Information unless we provide users with advance notice. This does not include website hosting partners and other parties who assist us in operating our website, conducting our business, or serving our users, so long as those parties agree to keep this information confidential. We may also release information when it&apos;s release is appropriate to comply with the law, enforce our site policies, or protect ours or others&apos; rights, property or safety.</p>
             <p>However, non-personally identifiable visitor information may be provided to other parties for marketing, advertising, or other uses.</p>
+            <h3>Third-Party Behavioral Tracking</h3>
+            <p>We use Google Analytics&apos; 3rd-party audience data such as age, gender, and interests to better understand the behavior of our customers and work with companies that collect information about your online activities to provide advertising targeted to suit your interests and preferences. These companies also use automated technologies to collect information when you click on our ads, which helps track and manage the effectiveness of our marketing efforts.</p>
+            <p>You may opt out of the automated collection of information by third-party ad networks for the purpose of delivering advertisements tailored to your interests, by visiting the consumer opt-out page for the Self-Regulatory Principles for Online Behavioral Advertising at <a target="_blank" rel="noopener noreferrer" className="break-word" href="http://www.aboutads.info/choices/">http://www.aboutads.info/choices/</a> and edit or opt-out your Google Display Netowrk ads&apos; preferences at <a target="_blank" rel="noopener noreferrer" className="break-word" href="http://www.google.com/ads/preferences/">http://www.google.com/ads/preferences/</a>.</p>
             <h3>Third-party links</h3>
             <p>We do not include or offer third-party products or services on our website.</p>
-            <h3>Google</h3>
-            <p>Google&apos;s advertising requirements can be summed up by Google&apos;s Advertising Principles. They are put in place to provide a positive experience for users. https://support.google.com/adwordspolicy/answer/1316548?hl=en</p>
+            <h3>Google AdWords</h3>
+            <p>This website uses the Google AdWords remarketing service to advertise on third party websites (including Google) to previous visitors to our site. It could mean that we advertise to previous visitors who haven&apos;t completed a task on our site. This could be in the form of an advertisement on the Google search results page, or a site in the Google Display Network. Third-party vendors, including Google, use cookies to serve ads based on someone&apos;s past visits to the Slide Therapy website. Of course, any data collected will be used in accordance with our own privacy policy and Google’s privacy policy.</p>
+            <p>We have enabled the following Google Analytics advertising features:</p>
+            <ul>
+              <li>Remarketing with Google Analytics</li>
+              <li>Google Display Network Impression Reporting</li>
+              <li>Google Analytics Demographics and Interest Reporting</li>
+              <li>Integrated services that require Google Analytics to collect data for advertising purposes, including the collection of data via advertising cookies and identifiers.</li>
+            </ul>
+            <p>Google&apos;s advertising requirements can be summed up by Google&apos;s Advertising Principles. They are put in place to provide a positive experience for users. <a target="_blank" rel="noopener noreferrer" className="break-word" href="https://support.google.com/adwordspolicy/answer/1316548?hl=en">https://support.google.com/adwordspolicy/answer/1316548?hl=en</a></p>
             <p>We have not enabled Google AdSense on our site but we may do so in the future.</p>
             <h3>California Online Privacy Protection Act</h3>
-            <p>CalOPPA is the first state law in the nation to require commercial websites and online services to post a privacy policy. The law&apos;s reach stretches well beyond California to require any person or company in the United States (and conceivably the world) that operates websites collecting Personally Identifiable Information from California consumers to post a conspicuous privacy policy on its website stating exactly the information being collected and those individuals or companies with whom it is being shared. See more at: http://consumercal.org/california-online-privacy-protection-act-caloppa/#sthash.0FdRbT51.dpuf</p>
+            <p>CalOPPA is the first state law in the nation to require commercial websites and online services to post a privacy policy. The law&apos;s reach stretches well beyond California to require any person or company in the United States (and conceivably the world) that operates websites collecting Personally Identifiable Information from California consumers to post a conspicuous privacy policy on its website stating exactly the information being collected and those individuals or companies with whom it is being shared. See more at: <a target="_blank" rel="noopener noreferrer" className="break-word" href="http://consumercal.org/california-online-privacy-protection-act-caloppa/#sthash.0FdRbT51.dpuf">http://consumercal.org/california-online-privacy-protection-act-caloppa/#sthash.0FdRbT51.dpuf</a></p>
             <h3>According to CalOPPA, we agree to the following:</h3>
             <p>Users can visit our site anonymously.</p>
             <p>Once this privacy policy is created, we will add a link to it on our home page or as a minimum, on the first significant page after entering our website.</p>
@@ -69,10 +83,6 @@ export default class Privacy extends React.Component {
             <ul>
               <li>By emailing us</li>
             </ul>
-            <h3>How does our site handle Do Not Track signals?</h3>
-            <p>We honor Do Not Track signals and Do Not Track, plant cookies, or use advertising when a Do Not Track (DNT) browser mechanism is in place.</p>
-            <h3>Does our site allow third-party behavioral tracking?</h3>
-            <p>It&apos;s also important to note that we do not allow third-party behavioral tracking</p>
             <h3>COPPA (Children Online Privacy Protection Act)</h3>
             <p>When it comes to the collection of personal information from children under the age of 13 years old, the Children&apos;s Online Privacy Protection Act (COPPA) puts parents in control. The Federal Trade Commission, United States&apos; consumer protection agency, enforces the COPPA Rule, which spells out what operators of websites and online services must do to protect children&apos;s privacy and safety online.</p>
             <p>We do not specifically market to children under the age of 13 years old.</p>
