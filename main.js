83e6bf0f83ab1ced0c357a3888c801217041f858
Jimmy Byrum <me@jimmybyrum.com>
2023-07-17 17:30:51 +0200

    Switch to Free GH Pages Version (#41)
    * redoing site with jekyll and no libs or frameworks (which feels great)
    
    * removing _site dir
    
    * adding _site to .gitignore
    
    * using vars more
    
    * removing unused css
    
    * adding previews
    
    * bug fixing with previews
    
    * taking away the buy button
    
    * bugfixing the carousel
    
    * getting all those escapes
    
    * simplifying
    
    * remove swiper stuff
    
    * remove swiper stuff
    
    * Create CNAME

---
+++ main.js
@@ -0,0 +1,143 @@
+const heroImages = [
+  '/i/home/topimage1.jpg',
+  '/i/home/topimage4.jpg',
+  '/i/home/topimage5.jpg'
+];
+
+const lastSlide = {
+  large: 31,
+  small: 27,
+  solitary: 28
+};
+
+const loaded = {
+  large: 1,
+  small: 1,
+  solitary: 1
+};
+
+let videoPlayer;
+
+function main() {
+  setupYouTube();
+  addScripts();
+  attachEvents();
+  pickHeroImage();
+  setupSwiper('.quotes .swiper-container', 'quotes');
+  setupPreviews();
+}
+
+function pickHeroImage() {
+  const imgUrl = heroImages[Math.floor(Math.random() * heroImages.length)];
+  const img = new Image();
+  img.src = imgUrl;
+  img.onload = function() {
+    document.querySelector('#hero').style.backgroundImage='url('+img.src+')';
+  };
+}
+
+function prepareVideo() {
+  if (!videoPlayer) {
+    videoPlayer = new YT.Player('video-player', {
+      events: {
+        onReady: e => {
+          startVideo();
+        },
+        onStateChange: e => {
+          if (e) {
+            // console.log(e.data, YT.PlayerState.ENDED)
+            if (e.data === YT.PlayerState.ENDED) {
+              // console.log('Video Done')
+              this.exitFullscreen().then(() => closeVideo());
+            }
+          }
+        }
+      }
+    });
+  } else {
+    startVideo();
+  }
+}
+
+function startVideo() {
+  // console.info('startVideo')
+  // https://developers.google.com/youtube/player_parameters#Parameters
+  if (videoPlayer) {
+    if (typeof videoPlayer.playVideo !== 'function') {
+      // seems like sometimes we get into a state where videoPlayer
+      // is defined but videoPlayer.playVideo isn't ready yet.
+      // Fine. We'll just try again in a few moments.
+      setTimeout(() => startVideo(), 100);
+    } else {
+      showVideoLayer();
+      videoPlayer.playVideo();
+    }
+  } else {
+    setTimeout(() => startVideo(), 500);
+  }
+}
+
+function showVideoLayer() {
+  document.querySelector('#video-player-layer').style.display = 'grid';
+}
+function hideVideoLayer() {
+  document.querySelector('#video-player-layer').style.display = 'none';
+}
+function videoLayerOpen() {
+  return document.querySelector('#video-player-layer').style.display !== 'none';
+}
+
+function attachEvents() {
+  const videoPlayerButton = document.querySelector('#startIntroVideo');
+  if (videoPlayerButton) {
+    videoPlayerButton.addEventListener('click', function onVideoPlayerButtonClick(e) {
+      prepareVideo();
+    }, {
+      passive: true
+    });
+  }
+
+  const videoPlayerCloseButton = document.querySelector('#video-player-layer .close-video-player');
+  if (videoPlayerCloseButton) {
+    videoPlayerCloseButton.addEventListener('click', function onVideoPlayerCloseButtonClick(e) {
+      closeVideo();
+    }, {
+      passive: true
+    });
+  }
+
+  window.addEventListener('keydown', function onKeyDown(e) {
+    if (e.keyCode === 27) {
+      if (videoLayerOpen()) {
+        closeVideo();
+      }
+    }
+  }, {
+    passive: true
+  });
+}
+
+function addScripts() {
+  setTimeout(() => {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  });
+}
+
+// https://developers.google.com/youtube/iframe_api_reference
+function setupYouTube() {
+  window.onYouTubeIframeAPIReady = () => {
+    document.querySelector('#startIntroVideo').style.visibility = 'visible';
+  }
+}
+
+function closeVideo() {
+  if (videoPlayer) {
+    videoPlayer.stopVideo();
+    hideVideoLayer();
+  }
+}
+
+main();
